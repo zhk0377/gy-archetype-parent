bbefb5821f12ae4be2476f2e109385b22c3735aa
guanyang <guanyangsunlight@163.com>
2016-04-21 00:08:12 +0800

    update

---
+++ gy-archetype-admin-web/src/main/resources/archetype-resources/src/main/java/controller/TestController.java
@@ -0,0 +1,30 @@
+package ${package}.controller;
+
+import java.util.Date;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.json.MappingJacksonJsonView;
+
+@Controller
+@RequestMapping("/test")
+public class TestController {
+
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    public ModelAndView test() {
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("sample/test.ftl");
+        mav.addObject("time", new Date());
+        return mav;
+    }
+
+    @RequestMapping(value = "/json", method = RequestMethod.GET)
+    public ModelAndView json() {
+        ModelAndView mav = new ModelAndView(new MappingJacksonJsonView());
+        mav.addObject("time", new Date());
+        return mav;
+    }
+
+}
--- gy-archetype-admin-web/src/main/resources/archetype-resources/src/main/webapp/WEB-INF/freemarker/sample/decorators/main.ftl
+++ gy-archetype-admin-web/src/main/resources/archetype-resources/src/main/webapp/WEB-INF/freemarker/sample/decorators/main.ftl
@@ -3,11 +3,11 @@
 <html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="zh-CN">
 	<head>
 		<title>${title}</title>
-		${head}
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<meta http-equiv="Cache-Control" content="no-transform" />
 		<meta name="applicable-device" content="pc">
-		</head>
+		${head}
+	</head>
 	<body>
         ${body}
 	</body>
